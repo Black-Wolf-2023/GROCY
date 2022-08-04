cf93059b98aebaee57d66615d9f05d5b10df57b3
Abdalrhman8000 <abdo.eslam2021@gmail.com>
2022-08-04 09:25:48 +0200

    adding and fixed

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -63,7 +63,7 @@ activtiy.ActiveOpions();
 //============= Activity || Class================>
 class Animations{
     Animater(img: HTMLElement, sec: HTMLElement) {
-        sec.style.display = "none";
+        // sec.style.display = "none";
         let srcs = [
             './images/load/1.svg',
             './images/load/2.svg',
